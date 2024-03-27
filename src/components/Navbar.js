1752f870c5f9fa7c9c538524629b0ace00f611ec
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-03-27 07:45:09 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,52 @@
+import { useState,useEffect } from "react";
+import { Nav,Navbar,Container } from "react-bootstrap";
+
+export const Navbar = () =>{
+    const [activeLink,setActiveLink] = useState('home')
+    const [scroll, setScroll] = useState(false)
+
+    useEffect (()=>{
+        const onScroll = ()=>{
+            if(window.scrollY > 50){
+                setScroll(true)
+            }else{
+                setScroll(false)
+            }
+        }
+         window.addEventListener("scroll", onScroll);
+
+         return (window.removeEventListener("scroll" , onScroll));
+    },[])
+
+    const updateActiveLink = (value)=>{
+        setActiveLink(value)
+    }
+
+    return (
+        <Navbar expand="lg" className={scroll? "scroll" : ""}>
+          <Container>
+            <Navbar.Brand href="#home">
+                <img src = {''} alt="Logo"/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" >
+                <span className="NavBar-toggle-icon"></span>
+            </Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home" className={activeLink? "active-link" : "nabar-link"} onClick={updateActiveLink('home')}>Home</Nav.Link>
+                <Nav.Link href="#skills" className={activeLink? "active-link" : "nabar-link"} onClick={updateActiveLink('skills')}>Skills</Nav.Link>
+                <Nav.Link href="#projects" className={activeLink? "active-link" : "nabar-link"} onClick={updateActiveLink('projects')}>Porjects</Nav.Link>
+              </Nav>
+              <span className="navbar-socials">
+                <div className="social-icons">
+                    <a href="#" alt='' ><img src={''} alt=""/></a>
+                    <a href="#" alt='' ><img src={''} alt=""/></a>
+                    <a href="#" alt='' ><img src={''} alt=""/></a>
+                </div>
+                <button className="navbar-connect-button" onClick={()=> (console.log('connect'))}><span> Let's Connect!! </span></button>
+              </span>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+}
