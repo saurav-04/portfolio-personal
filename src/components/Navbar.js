4f890d1e39b93acf5ae6bb21250352477415f5f6
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-03-27 23:51:14 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,27 +28,35 @@ export const NavBar = () =>{
 
     return (
         <Navbar expand="lg" className={scroll? "scroll" : ""}>
+          
           <Container>
+            
             <Navbar.Brand href="#home">
                 <img src = {logo} alt="Logo"/>
             </Navbar.Brand>
+            
             <Navbar.Toggle aria-controls="basic-navbar-nav" >
                 <span className="NavBar-toggle-icon"></span>
             </Navbar.Toggle>
+            
             <Navbar.Collapse id="basic-navbar-nav">
+              
               <Nav className="me-auto">
                 <Nav.Link href="#home" className={activeLink === 'home'? "active-link" : "navbar-link"} onClick={()=>updateActiveLink('home')}>Home</Nav.Link>
                 <Nav.Link href="#skills" className={activeLink=== 'skills' ? "active-link" : "navbar-link"} onClick={()=>updateActiveLink('skills')}>Skills</Nav.Link>
                 <Nav.Link href="#projects" className={activeLink === 'projects' ? "active-link" : "navbar-link"} onClick={()=>updateActiveLink('projects')}>Projects</Nav.Link>
               </Nav>
+              
               <span className="navbar-socials">
                 <div className="social-icons">
-                    <a href="#" alt='' ><img src={navIconn1} alt=""/></a>
-                    <a href="#" alt='' ><img src={navIconn2} alt=""/></a>
-                    <a href="#" alt='' ><img src={navIconn3} alt=""/></a>
+                    <a href="#linkedin" alt='linkedin' ><img src={navIconn1} alt="icon1"/></a>
+                    <a href="#facebook" alt='facebook' ><img src={navIconn2} alt="icon2"/></a>
+                    <a href="#instagram" alt='instagram' ><img src={navIconn3} alt="icon3"/></a>
                 </div>
-                <button className="navbar-connect-button" onClick={()=> (console.log('connect'))}><span> Let's Connect!! </span></button>
               </span>
+              
+              <button className="navbar-connect-button" onClick={()=> (console.log('connect'))}><span> Let's Connect!! </span></button>
+            
             </Navbar.Collapse>
           </Container>
         </Navbar>
