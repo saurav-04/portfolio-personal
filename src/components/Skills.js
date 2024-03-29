bb0d0e73f60a929dd1c2a1047306b885b075b808
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-03-29 23:33:54 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,3 +1,43 @@
+import { Col, Container, Row } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 export const Skills = ()=>{
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+
+      return(
+        <section className="skills" id="skills">
+            <Container>
+                <Row>
+                    <Col>
+                    <div className="skills-box">
+                        <h2>
+                            Skills
+                        </h2>
+                        <p>Duis commodo cupidatat do ut enim aliqua.</p>
+                    </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+      )
     
 }
