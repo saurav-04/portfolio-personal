268831629cb6d14e86c2da3d75f1bf358a0ba76c
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-03-29 23:45:41 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,7 @@ import "react-multi-carousel/lib/styles.css";
 export const Skills = ()=>{
     const responsive = {
         superLargeDesktop: {
-            
+
           breakpoint: { max: 4000, min: 3000 },
           items: 5
         },
@@ -34,7 +34,26 @@ export const Skills = ()=>{
                         </h2>
                         <p>Duis commodo cupidatat do ut enim aliqua.<br/></p>
                         <Carousel responsive={responsive} infinite = {true} className="skill-slider">
-
+                            <div className="item">
+                                <img src={''} alt="skills-image"/>
+                                <h5>HTML5/CSS3</h5>
+                            </div>
+                            <div className="item">
+                                <img src={''} alt="skills-image"/>
+                                <h5>JavaScript</h5>
+                            </div>
+                            <div className="item">
+                                <img src={''} alt="skills-image"/>
+                                <h5>React.js</h5>
+                            </div>
+                            <div className="item">
+                                <img src={''} alt="skills-image"/>
+                                <h5>Version Control</h5>
+                            </div>
+                            <div className="item">
+                                <img src={''} alt="skills-image"/>
+                                <h5>Problem-solving and Debugging</h5>
+                            </div>
                         </Carousel>
                     </div>
                     </Col>
