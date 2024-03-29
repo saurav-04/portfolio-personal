47d9d79ea6c4efc52107b019fce622b85dfa91bd
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-03-29 12:42:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { NavBar } from './components/Navbar';
 import {Banner} from './components/Banner';
+import {Skills} from './components/Skills'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Banner/>
+      <Skills/>
     </div>
   );
 }
