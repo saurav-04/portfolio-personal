a8d110c792d98ebcc9688214857fb1a65e1f3865
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-03-30 12:25:12 +0530

    Update ProjectsCards.js

--- src/components/ProjectsCards.js
+++ src/components/ProjectsCards.js
@@ -3,9 +3,9 @@ import { Col } from "react-bootstrap"
 export const ProjectsCards = ({tittle,description,imgurl}) =>{
     return(
         <Col sm={6} md={4}>
-            <div className="proj-imgbox">
+            <div className="proj-imgbx">
                 <img src={imgurl} alt="projImg"/>
-                <div className="proj-txt">
+                <div className="proj-txtx">
                     <h4>{tittle}</h4>
                     <span>{description}</span>
                 </div>
