0459298d3027aed3cb1b314a8f913e283ad67e76
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-03-30 17:20:58 +0530

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -38,11 +38,27 @@ export const ContactForm = ()=>{
                         <form>
                             <Row>
                                 <Col md={6} className="px-1">
-                                    <input type="text" value={formDetails.firstName} placeholder="Firstname" onChange={(e)=>{updateFromdetails('firstName',e.target.value)}}/>
+                                    <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e)=>{updateFromdetails('firstName',e.target.value)}}/>
                                 </Col>
                                 <Col md={6} className="px-1">
-                                <input type="text" value={formDetails.lastName} placeholder="Lastname" onChange={(e)=>{updateFromdetails('lastName', e.target.value)}}/>
+                                <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e)=>{updateFromdetails('lastName', e.target.value)}}/>
                                 </Col>
+                                <Col md={6} className="px-1">
+                                <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e)=>{updateFromdetails('email',e.target.value)}}/>
+                                </Col>
+                                <Col md={6} className="px-1">
+                                <input type="tel" value={formDetails.phone} placeholder="Phone Number" onChange={(e)=>{updateFromdetails('phone',e.target.value)}}/>
+                                </Col>
+                                <Col>
+                                   <textarea row={6} value={formDetails.message} placeholder="Message" onChange={(e)=>{updateFromdetails('phone',e.target.value)}}/>
+                                   <button type="submit"><span>{buttonText}</span></button>
+                                </Col>
+                                {
+                                    status.message && 
+                                    <Col>
+                                      <p className={status.success === true ? "success" : "danger"}>{status.message}</p>
+                                    </Col>
+                                }
                             </Row>
                         </form>
                     </Col>
