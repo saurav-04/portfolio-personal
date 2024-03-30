f129ac8aa334a3b1e242001cdeee4a155734e7b7
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-03-30 12:25:15 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,84 +1,92 @@
-import { Container, Row ,Col, Nav, Tab} from "react-bootstrap";
+
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectsCards } from "./ProjectsCards";
-import colorSharp2 from '../assets/img/color-sharp2.png'
-import projImg1 from '../assets/img/project-img1.png'
-import projImg2 from '../assets/img/project-img2.png'
-import projImg3 from '../assets/img/project-img3.png'
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
 
-export const Projects = ()=>{
-    const projects  = [
-        {
-            tittle:"Project1",
-            description : "Description1",
-            imgurl: projImg1,
-        },
-        {
-            tittle:"Project2",
-            description : "Description2",
-            imgurl: projImg2,
-        },
-        {
-            tittle:"Project3",
-            description : "Description3",
-            imgurl: projImg3,
-        },
-        {
-            tittle:"Project4",
-            description : "Description4",
-            imgurl: projImg1,
-        },
-        {
-            tittle:"Project5",
-            description : "Description5",
-            imgurl: projImg2,
-        },
-        {
-            tittle:"Project6",
-            description : "Description6",
-            imgurl: projImg3,
-        },
-    ];
+export const Projects = () => {
 
-    return (
-        <section className="projects" id="projects">
-            <Container>
-                <Row>
-                    <Col>
-                       <h2>Projects</h2>
-                       <p>Enim officia dolore esse mollit consectetur quis veniam nulla nisi ipsum sunt.</p>
-                       <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                       <Nav variant="tabs" defaultActiveKey="/home">
-                            <Nav.Item>
-                                <Nav.Link eventKey="first">Tab One </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="second">Tab Two</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="third"> Tab Three</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                        <Tab.Content>
-                            <Tab.Pane eventKey="first">
-                                {
-                                    projects.map((projects,index) =>{
-                                       return(
-                                        <p>
-                                            <ProjectsCards key={index} {...projects}/>
-                                        </p>
-                                       )
-                                    })
-                                }
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="second">Lorem epsum</Tab.Pane>
-                            <Tab.Pane eventKey="third">Lorem epsum</Tab.Pane>
-                        </Tab.Content>
-                        </Tab.Container>
-                    </Col>
-                </Row>
-            </Container>
-            <img className="background-image-right" src={colorSharp2} alt="bgImageRight"/>
-        </section>
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgurl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgurl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgurl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgurl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgurl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgurl: projImg3,
+    },
+  ];
 
-    )
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+                <h2>Projects</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content >
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectsCards
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2} alt="background-iange-right"></img>
+    </section>
+  )
 }
